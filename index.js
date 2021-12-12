91b779bae2044666fb3088308c61767ab198185d
Katherine Jean-Charles <katherinejc103@gmail.com>
2021-12-12 12:14:27 -0500

    Done

---
+++ index.js
@@ -0,0 +1,25 @@
+let num1= 15
+function add(num1) {
+return num1 += 1;
+}
+let num2 = 5
+function subtract(num2) {
+    return num2 - 1;
+}
+let num3 = 20
+function multiply(num1, num3) {
+    return num1 * num3;
+}
+let num4 = 100
+function divide(num4, num3) {
+    return num4 / num3;
+}
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num3, num2) {
+    return num3 - num2;
+}
+function increment(num4) {
+    return num4++;
+}
